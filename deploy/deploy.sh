47545b10aa57cddd1378d88add4d5f129082cbd3
Marc Burns <m4burns@uwaterloo.ca>
2016-10-15 17:55:41 -0400

    Fix relative path, remove debug print

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync ../frontend s3://sotellme.ca/ --exclude '.git' --exclude '.gitignore' --delete
+aws s3 sync frontend s3://sotellme.ca/ --exclude '.git' --exclude '.gitignore' --delete
